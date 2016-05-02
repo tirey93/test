438fea2f52836de7269f0c8d80c54ae820894de1
tirey93 <tirey93@gmail.com>
2016-05-02 15:26:51 +0200

    funkcja1

---
+++ funkcja.c
@@ -0,0 +1,18 @@
+void main()
+{
+    int arr1[SIZE],arr2[SIZE][5],s1,i,j;
+    drawNum(arr1);
+    for(i=0;i<SIZE;i++)
+        printf("%d\n",arr1[i]);
+    splitNumOnDigits(arr1,arr2);
+    printf("\n");
+    for(i=0;i<SIZE;i++){
+        for(j=0;j<5;j++)
+            printf("%d ",arr2[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+    joinDigitsOnNum(arr2,arr1);
+    for(i=0;i<SIZE;i++)
+        printf("%d\n",arr1[i]);
+}
